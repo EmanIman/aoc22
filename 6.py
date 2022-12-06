c1b89a37d79af69ef17dba3ee55e3a63b6844003
Emanuel <emanuel.sjovall@gmail.com>
2022-12-06 15:43:19 +0100

    improved way to get the window

--- 6.py
+++ 6.py
@@ -1,22 +1,19 @@
 def loadData() -> str:
-    data = []
     with open("6i.txt", "r") as f:
         return f.readline()
 
 
 def part1(data: str) -> int:
     for i in range(len(data) - 3):
-        s = set([data[i], data[i + 1], data[i + 2], data[i + 3]])
+        s = set(data[i:i+4])
         if len(s) == 4:
             # i is index of the first character in the four character sequence and we want index of the last character
             return i + 4
 
 
 def part2(data: str) -> int:
-    for i in range(len(data) - 3):
-        s = set()
-        for j in range(14):
-            s.add(data[i + j])
+    for i in range(len(data) - 13):
+        s = set(data[i:i+14])
         if len(s) == 14:
             # same thing but sequence is 14 long
             return i + 14
